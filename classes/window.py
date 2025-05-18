7ebc11ff7dfb6910e2b60885bee28535f5fadcd5
Joris van Rijn <jorisvanrijn2006@gmail.com>
2025-05-18 14:13:56 +0200

    added 2 new screens 1 for loging in and one for making projects :) (no backend yet)

--- classes/window.py
+++ classes/window.py
@@ -8,8 +8,26 @@ class Window:
         self.frame = Frame(self.root)
         self.frame.pack(fill="both", expand=True)
         self.root.geometry(f"{width}x{height}")
-        self.create_widgets()
+        # self.login_screen()
+        self.project_screen()
         self.root.mainloop()
+        
+    #screens
+
+    def login_screen(self):
+        self.frame.pack(fill="both", expand=True)
+        self.addLabel("Login", 10)
+        self.addEntry("Username", 10)
+        self.addEntry("Password", 10)
+        self.addButton("Login", self.login, 10)
+
+    def project_screen(self):
+        self.frame.pack(fill="both", expand=True)
+        self.addLabel("Project", 10)
+        self.addButton("Create Project", self.create_project, 10)
+
+
+    ## widgets
 
     def addButton(self, text: str, command, pady: int):
         self.button = Button(self.frame, text=text, command=command)
@@ -30,5 +48,13 @@ class Window:
         self.addLabel("Welcome to the Scrum Tool", 10)
         self.addButton("Click Me", self.on_button_click, 10)
 
+    # button functions
+
     def on_button_click(self):
         messagebox.showinfo("Information", "Button clicked!")
+
+    def login(self):
+        print("Login button clicked")
+
+    def create_project(self):
+        print("Create Project button clicked")
