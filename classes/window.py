d9326b993ead7b5d378ba5103b33f5948559f648
Joris van Rijn <jorisvanrijn2006@gmail.com>
2025-05-18 13:58:11 +0200

    added basic window

--- classes/window.py
+++ classes/window.py
@@ -2,17 +2,16 @@ from tkinter import Tk, Frame, Label, Button, StringVar, messagebox
 from tkinter import ttk
 
 class Window:
-    def __init__(self, master):
-        self.master = master
-        self.master.title("Scrum Tool")
-        self.master.geometry("600x400")
-
+    def run(self, title: str, width: int, height: int):
+        self.root =  Tk()
+        self.root.title(title)
+        self.frame = Frame(self.root)
+        self.frame.pack(fill="both", expand=True)
+        self.root.geometry(f"{width}x{height}")
         self.create_widgets()
+        self.root.mainloop()
 
     def create_widgets(self):
-        self.frame = Frame(self.master)
-        self.frame.pack(fill="both", expand=True)
-
         self.label = Label(self.frame, text="Welcome to the Scrum Tool")
         self.label.pack(pady=10)
 
